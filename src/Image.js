7ab8cda0f1df2a981be0f7db936a045ba83354c4
Manik Dingra <manikdhingra0582@gmail.com>
2022-12-31 22:35:29 +0530

    Fade effect added

--- src/Image.js
+++ src/Image.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Image.css"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import Fade from 'react-reveal/Fade';
 function Image({ imgUrl, modelName, desc, rightButton }) {
     return (
         <div className='image' style={{
@@ -12,16 +12,21 @@ function Image({ imgUrl, modelName, desc, rightButton }) {
             height: '100vh',
             width: '100vw',
         }}>
-            <div className="container">
-                <h1 className='container__modelName'>{modelName}</h1>
-                <a href="http://" >{desc}</a>
-            </div>
-            <div className="container2">
-                <div className="btn">
-                    <button className='btn__buyNow' style={{ padding: "10px 97px" }}>Buy Now</button>
-                    {rightButton && <button className='btn__custom'>{rightButton}</button>}
+            <Fade bottom>
+
+                <div className="container">
+                    <h1 className='container__modelName'>{modelName}</h1>
+                    <a href="http://" >{desc}</a>
                 </div>
-                <ExpandMoreIcon className='icon' />
+            </Fade>
+            <div className="container2">
+                <Fade bottom>
+                    <div className="btn">
+                        <button className='btn__buyNow' style={{ padding: "10px 97px" }}>Buy Now</button>
+                        {rightButton && <button className='btn__custom'>{rightButton}</button>}
+                    </div>
+                    <ExpandMoreIcon className='icon' />
+                </Fade>
             </div>
 
         </div>
